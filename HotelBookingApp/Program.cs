3347a92920a8afe0669fb71e5614ef75fcdb594c
komarbv211 <andrijkoslak@gmail.com>
2025-10-08 20:08:10 +0300

    Fix: remove submodule and add HotelBookingApp as regular folder

---
+++ HotelBookingApp/Program.cs
@@ -0,0 +1,27 @@
+using Application;
+using Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Application + Infrastructure DI
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
